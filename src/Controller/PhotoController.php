4a382af1386a67a589b64b11f1938e73d43729b7
Randy <rmakaiadevgmail.com>
2026-05-07 12:58:09 +0200

    Verouillage de l'admin et mis en place d'un filtre

--- src/Controller/PhotoController.php
+++ src/Controller/PhotoController.php
@@ -6,27 +6,35 @@ use App\Entity\Photo;
 use App\Repository\CategoryRepository;
 use App\Repository\PhotoRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
 final class PhotoController extends AbstractController
 {
     #[Route('/', name: 'app_photo')]
-    public function index(PhotoRepository $photoRepository ,CategoryRepository $catego): Response
+    public function index(Request $request, PhotoRepository $photoRepository, CategoryRepository $catego): Response
     {
-        $photos = $photoRepository->findAll();  
+        $categoryId = $request->query->get('category');
+
+        if ($categoryId) {
+            $photos = $photoRepository->findBy(['category' => $categoryId]);
+        } else {
+            $photos = $photoRepository->findAll();
+        }
+
         return $this->render('photo/index.html.twig', [
             'photos' => $photos,
-            'categories'=>$catego->findAll()
+            'categories' => $catego->findAll(),
+            'currentCategory' => $categoryId,
         ]);
     }
 
-    #[Route('photo/{id}',name:'app_show', methods:['GET'])]
-    public function show(Photo $photo)
+    #[Route('photo/{id}', name: 'app_show', methods: ['GET'])]
+    public function show(Photo $photo): Response
     {
-        return $this->render ('photo/show.html.twig',[
-            'photo'=>$photo,
+        return $this->render('photo/show.html.twig', [
+            'photo' => $photo,
         ]);
     }
-
 }
